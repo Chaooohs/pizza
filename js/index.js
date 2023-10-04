5f18774b8347cfb769e356ac38d19790883c567a
Chaooohs <01nikka01@gmail.com>
2023-10-04 21:00:28 +0300

    asdf

--- js/index.js
+++ js/index.js
@@ -10,40 +10,177 @@ const reset = el('input[type="reset"]')
 const draggable = all('.draggable')
 
 
-class Cost {
-  constructor(price) {
-    this.price = price
-  }
+// import of a file with a product catalog
+async function request(url) {
+  const data = await fetch(url)
+  return await data.json()
+}
 
-  sizePrice() {
-    el('#price').innerText = this.price + ' грв'
-  }
+// import of a file with a product catalog
+request('./public/product-list.json')
+  .then((json) => outJson(json))
+  .catch((e) => console.error('error'))
+
+
+let product = []
+
+function outJson(json) {
+  product = json
 }
 
 
-const sauceClassic = new Cost(25)
-const sauceBBQ = new Cost(25)
-const sauceRikotta = new Cost(25)
-const moc1 = new Cost(30)
-const moc2 = new Cost(35)
-const moc3 = new Cost(40)
-const telya = new Cost(50)
-const vetch1 = new Cost(15)
-const vetch2 = new Cost(30)
+// object with ingredients
+const choiceUser = {
+  size: '',
+  sauce: [],
+  topping: [],
+  price: '0'
+}
 
 
-el('#small').addEventListener('click', function (e) {
-  const small = new Cost(50)
-  small.sizePrice()
+// choise of size
+el('#pizza').addEventListener('click', function (e) {
+  if (e.target.classList.contains('radioin')) {
+    selectUser(e.target)
+  }
 })
-el('#mid').addEventListener('click', function (e) {
-  const mid = new Cost(75)
-  mid.sizePrice()
+
+
+// filter of ingredients
+function selectUser(choise) {
+  if (choise.name === 'size') choiceUser.size = product.size.find((item) => item.productSize === choise.value)
+  if ('sauceClassicsauceBBQsauceRikotta'.includes(choise)) choiceUser.sauce.push(product.sauce.find((item) => item.productID === choise))
+  if ('moc1moc2moc3telyavetch1vetch2'.includes(choise)) choiceUser.topping.push(product.topping.find((item) => item.productID === choise))
+  calculate(choiceUser)
+}
+
+
+// cost calculation
+function calculate(elem) {
+  let price = 0
+
+  if (elem.size !== '') {
+    price += elem.size.productCost
+  }
+  if (elem.sauce.length > 0) {
+    for (let key of elem.sauce) {
+      price += key.productCost
+    }
+  }
+  if (elem.topping.length > 0) {
+    for (let key of elem.topping) {
+      price += key.productCost
+    }
+  }
+  choiceUser.price = price
+  showCost(choiceUser.price)
+}
+
+// price display on screen
+const showCost = (num) => el('#price').innerText = num + ' грв'
+showCost(choiceUser.price)
+
+
+// movable element
+draggable.forEach((item) => {
+  item.addEventListener('dragstart', function (e) {
+    e.dataTransfer.setData('id', e.target.id)
+    e.dataTransfer.setData('name', e.target.dataset.name)
+    e.dataTransfer.setData('sauce', e.target.dataset.sauce)
+    e.dataTransfer.setData('topping', e.target.dataset.topping)
+  })
 })
-el('#big').addEventListener('click', function (e) {
-  const big = new Cost(100)
-  big.sizePrice()
+
+// override default action for parent
+el('.table').addEventListener('dragover', function (e) {
+  e.preventDefault()
+  // changing the mouse cursor
+  e.dataTransfer.dropEffect = 'copy';
 })
+
+// element release
+el('.table').addEventListener('drop', function (e) {
+  let id = e.dataTransfer.getData('id')
+  let name = e.dataTransfer.getData('name')
+  let sauce = e.dataTransfer.getData('sauce')
+  let topping = e.dataTransfer.getData('topping')
+
+  this.appendChild(document.getElementById(id))
+  // this.appendChild(document.getElementById(id).cloneNode(true))
+
+  selectUser(id)
+
+  if (sauce === 'sauce') {
+    el('#sauce').insertAdjacentHTML('beforeend', `
+        <div class="result__list" id="${id}">
+            <span class="result__item">${name}</span>
+            <img class="result__img" src="/img/close-gray.svg" alt="del">
+        </div>
+    `)
+    del()
+  }
+
+  if (topping === 'topping') {
+    el('#topping').insertAdjacentHTML('beforeend', `
+        <div class="result__list" id="${id}">
+            <span class="result__item">${name}</span>
+            <img class="result__img" src="/img/close-gray.svg" alt="del">
+        </div>
+    `)
+    del()
+  }
+})
+
+
+// function to remove from the list of toppings
+function del() {
+  el('.result').addEventListener('click', (e) => {
+    if (e.target.closest('.result__img')) {
+      const b = e.target.closest('.result__list')
+      choiceUser.sauce = choiceUser.sauce.filter((item) => item.productID !== b.id)
+      choiceUser.topping = choiceUser.topping.filter((item) => item.productID !== b.id)
+      b.remove()
+      calculate(choiceUser)
+      removeImg(b.id)
+    }
+  })
+}
+
+
+// function to remove elements from pizza
+function removeImg(b) {
+  all('img').forEach((img) => {
+    if (img.closest('.table')) {
+      if(b === 'sauceClassic' && img.id === 'sauceClassic'){
+        el('#a').insertAdjacentElement('afterBegin', img);
+      }
+      else if(b === 'sauceBBQ' && img.id === 'sauceBBQ'){
+        el('#b').insertAdjacentElement('afterBegin', img);
+      }
+      else if(b === 'sauceRikotta' && img.id === 'sauceRikotta'){
+        el('#c').insertAdjacentElement('afterBegin', img);
+      }
+      else if(b === 'moc1' && img.id === 'moc1'){
+        el('#d').insertAdjacentElement('afterBegin', img);
+      }
+      else if(b === 'moc2' && img.id === 'moc2'){
+        el('#e').insertAdjacentElement('afterBegin', img);
+      }
+      else if(b === 'moc3' && img.id === 'moc3'){
+        el('#f').insertAdjacentElement('afterBegin', img);
+      }
+      else if(b === 'telya' && img.id === 'telya'){
+        el('#g').insertAdjacentElement('afterBegin', img);
+      }
+      else if(b === 'vetch1' && img.id === 'vetch1'){
+        el('#h').insertAdjacentElement('afterBegin', img);
+      }
+      else if(b === 'vetch2' && img.id === 'vetch2'){
+        el('#i').insertAdjacentElement('afterBegin', img);
+      }
+    }
+  })
+}
 
 
 // verification of form
@@ -59,43 +196,8 @@ reset.addEventListener('click', resetForm)
 el('#btnSubmit').addEventListener('click', setForm)
 
 
-// movable element
-draggable.forEach((item) => {
-  item.addEventListener('dragstart', function (e) {
-    e.dataTransfer.setData('id', e.target.id)
-    e.dataTransfer.setData('sauce', e.target.dataset.sauce)
-    e.dataTransfer.setData('topping', e.target.dataset.topping)
-    e.dataTransfer.setData('name', e.target.dataset.name)
-  })
-})
 
-// override default action for parent
-el('.table').addEventListener('dragover', function (e) {
-  e.preventDefault()
-  // changing the mouse cursor
-  e.dataTransfer.dropEffect = 'copy';
-})
 
-// element release
-el('.table').addEventListener('drop', function (e) {
-  let id = e.dataTransfer.getData('id')
-  let sauce = e.dataTransfer.getData('sauce')
-  let topping = e.dataTransfer.getData('topping')
-  let itemName = e.dataTransfer.getData('name')
 
-  this.appendChild(document.getElementById(id).cloneNode(true))
 
-  if (sauce === 'sauce'){
-    const sauce = document.createElement('p')
-    sauce.classList.add("list");
-    sauce.innerText = itemName
-    el('#sauce').append(sauce)
-  }
-  else if (topping === "topping") {
-    const sauce = document.createElement('p')
-    sauce.classList.add("list");
-    sauce.innerText = itemName
-    el('#topping').append(sauce)
-  }
-})
 
